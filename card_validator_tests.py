192fe465552c935116b6a94a2782b9c27a75644d
TakenouchiTR <draconianlds@gmail.com>
2021-09-08 18:25:59 -0400

    Add test for parse_starting_digits

--- card_validator_tests.py
+++ card_validator_tests.py
@@ -55,3 +55,24 @@ class TestCreateNumberRange(unittest.TestCase):
 
         for i in range(number_one, number_two + 1):
             self.assertTrue(regex.match(str(i)))
+
+class TestParseStartingDigits(unittest.TestCase):
+    def test_single_number(self):
+        num_string = "32"
+        regex_string = card_validator.parse_starting_digits(num_string)
+        self.assertEqual(regex_string, "32")
+    
+    def test_two_numbers(self):
+        num_string = "32,35"
+        regex_string = card_validator.parse_starting_digits(num_string)
+        self.assertEqual(regex_string, "32|35")
+    
+    def test_number_range_no_similar_digits(self):
+        num_string = "1003-2375"
+        regex_string = card_validator.parse_starting_digits(num_string)
+        self.assertEqual(regex_string, "(100[3-9]|10[1-9][0-9]{1}|1[1-9][0-9]{2}|2[0-2][0-9]{2}|23[0-6][0-9]{1}|237[0-5])")
+    
+    def test_number_range_with_similar_digits(self):
+        num_string = "1003-1075"
+        regex_string = card_validator.parse_starting_digits(num_string)
+        self.assertEqual(regex_string, "(10(0[3-9]|[1-6][0-9]{1}|7[0-5]))")
