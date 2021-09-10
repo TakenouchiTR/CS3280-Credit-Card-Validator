64295c4a4819001f9b726aa345cfb6aaf10a94e2
TakenouchiTR <draconianlds@gmail.com>
2021-09-10 16:16:11 -0400

    Add shebang

--- cs3280project1.py
+++ cs3280project1.py
@@ -1,7 +1,7 @@
+#!/usr/bin/env python3
 import sys
 import re
 import csv
-
 import utils
 
 INVALID = "invalid"
--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3 
 import re
 
 def luhn_verified(card_number):
